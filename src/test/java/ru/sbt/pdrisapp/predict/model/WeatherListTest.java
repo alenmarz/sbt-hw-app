047796b843c88b7506500dac402fbc099f9a72cd
Alena Martsenyuk <a.martsenyuk@bslab.io>
2020-12-12 20:56:07 +0000

    Add predict tests

---
+++ src/test/java/ru/sbt/pdrisapp/predict/model/WeatherListTest.java
@@ -0,0 +1,21 @@
+package ru.sbt.pdrisapp.predict.model;
+
+import org.junit.Test;
+import ru.sbt.pdrisapp.weather.model.db.WeatherEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class WeatherListTest {
+
+    @Test
+    public void getWeather() {
+        WeatherList weatherList = new WeatherList();
+        List<WeatherEntity> weatherEntityList = new ArrayList<>();
+        weatherList.setWeather(weatherEntityList);
+        assertEquals(weatherList.getWeather(), weatherEntityList);
+    }
+
+}
